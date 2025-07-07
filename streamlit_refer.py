22b8980744280992f766b06eb4663bb8e56eb100
junlongpark <167068127+junlongpark@users.noreply.github.com>
2025-07-08 01:09:57 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -124,11 +124,8 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings(
-                                        model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                        model_kwargs={'device': 'cpu'},
-                                        encode_kwargs={'normalize_embeddings': True}
-                                        )  
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device="cpu")
+    embeddings = HuggingFaceEmbeddings(model_name=None, client=model)
     vectordb = FAISS.from_documents(text_chunks, embeddings)
     return vectordb
 
